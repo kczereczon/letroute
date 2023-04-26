a6f756cb5e00985906d753ce981106726f4beefe
Krzysztof Czereczon <kczereczon@gmail.com>
2023-04-26 19:40:54 +0200

    working with sets

---
+++ src/Controller/SetController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SetController extends AbstractController
+{
+    #[Route('/set', name: 'app_set')]
+    public function index(): Response
+    {
+        return $this->render('set/index.html.twig', [
+            'controller_name' => 'SetController',
+        ]);
+    }
+}
