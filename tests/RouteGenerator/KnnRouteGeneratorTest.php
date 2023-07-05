28151bb3d3c895ac94ee05978d448a2c829b4d0e
Krzysztof Czereczon <kczereczon@gmail.com>
2023-07-05 21:22:08 +0200

    added test for endless loop protection

--- tests/RouteGenerator/KnnRouteGeneratorTest.php
+++ tests/RouteGenerator/KnnRouteGeneratorTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\RouteGenerator;
 
+use App\Entity\Point;
 use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\TestCase;
 
@@ -26,6 +27,58 @@ class KnnRouteGeneratorTest extends TestCase
         $this->assertEquals([], $knnRouteGenerator->generate(
             $this->createMock(\App\Entity\Set::class),
             new \Doctrine\Common\Collections\ArrayCollection(),
+            0,
+            0,
+            0,
+            $this->createMock(\App\Interfaces\Coordinates::class)
+        )->toArray());
+    }
+
+    public function testLoopEndsWhenTriesExceeded()
+    {
+        $point = new Point();
+        $point->setX(51.912096);
+        $point->setY(19.344748);
+        $point->setWeight(2000);
+
+        $car = new \App\Models\Car(
+            'test',
+            1000,
+            1000,
+        );
+
+        $sorter = $this->createMock(\App\Sorter\EuclideanDistanceSorter::class);
+        $sorter
+            ->method('sort')
+            ->willReturn(new ArrayCollection([
+                $point
+            ]));
+
+        $pointRepository = $this->createMock(\App\Repository\PointRepository::class);
+        $pointRepository->expects($this->once())
+            ->method('getPointsWithoutRoute')
+            ->willReturn(new ArrayCollection([
+                $point
+            ]));
+
+        $distanceCalculator = $this->createMock(\App\DistanceCalculator\DistanceCalculatorInterface::class);
+
+        $durationCalculator = $this->createMock(\App\DurationCalculator\DurationCalculatorInterface::class);
+
+        $knnRouteGenerator = new \App\RouteGenerator\KnnRouteGenerator(
+            $pointRepository,
+            $durationCalculator,
+            $distanceCalculator,
+            $this->createMock(\App\Factory\RouteFactory::class),
+            $this->createMock(\App\Domain\CentroidRandomizerInterface::class),
+            $sorter
+        );
+
+        $this->assertEquals([], $knnRouteGenerator->generate(
+            $this->createMock(\App\Entity\Set::class),
+            new \Doctrine\Common\Collections\ArrayCollection([
+                $car
+            ]),
             0,
             0,
             0,
