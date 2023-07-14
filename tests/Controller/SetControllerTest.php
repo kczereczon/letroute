e515b84792892fd9f8c8a19ef3d2b337f0809c3d
Krzysztof Czereczon <kczereczon@gmail.com>
2023-07-14 06:09:13 +0200

    added new test

--- tests/Controller/SetControllerTest.php
+++ tests/Controller/SetControllerTest.php
@@ -3,15 +3,29 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class SetControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testUpdatingFile(): void
     {
+        $uploadedFile = new UploadedFile(
+            dirname(__DIR__) . '/TestFiles/testSet.csv',
+            'testSet.csv',
+            'text/csv',
+            null
+        );
+
         $client = static::createClient();
-        $crawler = $client->request('POST', '/');
+        $crawler = $client->request('POST', '/set/create', [
+            'headers' => ['Content-Type' => 'multipart/form-data'],
+        ],
+        [
+            'points' => $uploadedFile
+        ]);
 
         self::assertResponseIsSuccessful();
-        self::assertSelectorTextContains('h1', 'Hello World');
+        self::assertResponseRedirects('/set');
+        self::assertSelectorExists('a.set');
     }
 }
