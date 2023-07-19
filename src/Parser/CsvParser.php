fef95df0f56a996dc0cf098951e8a8be779659c9
Krzysztof Czereczon <kczereczon@gmail.com>
2023-07-19 11:12:58 +0200

    added format parser

--- src/Parser/CsvParser.php
+++ src/Parser/CsvParser.php
@@ -12,9 +12,8 @@ class CsvParser implements ParserInterface
     public function parse(string $data): array
     {
 
+        iconv(mb_detect_encoding($data, mb_detect_order(), true), "UTF-8", $data);
         $explodedString = preg_split('/\R/', $data);
-
-        iconv(mb_detect_encoding($explodedString, mb_detect_order(), true), "UTF-8", $text);
         $points = array_map('str_getcsv', $explodedString);
         array_walk($points, function(&$a) use ($points) {
             $a = array_combine($points[0], $a);
